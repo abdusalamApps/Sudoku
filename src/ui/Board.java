c7ffa851ac67f6a019e9332e8b20d4ee567b1309
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2019-12-07 14:03:04 +0100

    sole row

--- src/ui/Board.java
+++ src/ui/Board.java
@@ -1,8 +1,6 @@
 package ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -11,10 +9,13 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Board extends Application {
 
     static TextField[][] textFields;
     static int[][] intMatrix;
+    static boolean fail = false;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -54,7 +55,36 @@ public class Board extends Application {
 
         }
 
-        solveButton.setOnAction(e -> solveRow());
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            integers.add(i);
+            intMatrix[0][i - 1] = i;
+        }
+        intMatrix[0][3] = 0;
+        intMatrix[0][1] = 0;
+
+        System.out.print("List:   ");
+        integers.forEach(num -> System.out.print(num + " "));
+        System.out.println();
+        System.out.print("Matrix: ");
+        for (int i = 0; i < intMatrix.length; i++) {
+            System.out.print(intMatrix[0][i] + " ");
+        }
+        System.out.println();
+
+        solveButton.setOnAction(e -> {
+            while (containsZeros(intMatrix[0])) {
+                solveRow(0);
+            }
+
+            for (int k = 0; k < intMatrix.length; k++) {
+                System.out.print(intMatrix[0][k] + " ");
+            }
+            System.out.println();
+
+        });
+
+        System.out.print("New matrix: ");
 
         root.setCenter(tilePane);
         root.setTop(hBox);
@@ -63,10 +93,52 @@ public class Board extends Application {
         primaryStage.show();
     }
 
-    private static void solveRow() {
+    private static void checkRow(ArrayList<Integer> list, int index) {
+        if (!list.isEmpty() && index >= 0) {
+            int current = intMatrix[0][index];
+            if (list.contains(current)) {
+                System.out.println(current);
+//                System.out.println(list.remove(index));
+                list.remove((Object) current);
+                checkRow(list, list.size() - 1);
+            } else {
+                System.out.println("fail");
+            }
+        }
 
     }
 
+    private static void solveRow(int column) {
+        if (column <= 8) {
+            if (intMatrix[0][column] == 0) {
+                for (int i = 1; i <= 9; i++) {
+                    if (!arrayContains(intMatrix[0], i)) {
+                        intMatrix[0][column] = i;
+                    }
+                }
+            } else {
+                solveRow(column + 1);
+            }
+        }
+    }
+
+    private static boolean arrayContains(int[] array, int number) {
+        for (int value : array) {
+            if (number == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean containsZeros(int[] array) {
+        for (int value : array) {
+            if (value == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         launch(args);
