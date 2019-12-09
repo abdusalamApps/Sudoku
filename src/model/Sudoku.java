fdc97b469a4f08190ac0fd1ffa72456a5065269d
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2019-12-09 16:08:05 +0100

    another try

--- src/model/Sudoku.java
+++ src/model/Sudoku.java
@@ -1,12 +1,15 @@
 package model;
 
 
+import java.util.Random;
+
 public class Sudoku {
 
     int[][] matrix;
 
     public Sudoku(int[][] matrix) {
         this.matrix = matrix;
+        fillRandomly();
     }
 
     public int getBoxValue(int x, int y) {
@@ -27,5 +30,26 @@ public class Sudoku {
 
     private boolean solve(int i, int j) {
         return false;
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + ", ");
+
+            }
+
+        }
+    }
+
+    public void fillRandomly() {
+        for (int i = 0; i < 40; i++) {
+            Random random = new Random();
+            int positionX = random.nextInt(8);
+            int positionY = random.nextInt(8);
+            int element = random.nextInt(8);
+            this.matrix[positionX][positionY] = element;
+        }
+
     }
 }
--- src/ui/Board.java
+++ src/ui/Board.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import model.Sudoku;
+import org.apache.http.util.TextUtils;
 
 import java.util.ArrayList;
 
@@ -15,14 +17,16 @@ public class Board extends Application {
 
     static TextField[][] textFields;
     static int[][] intMatrix;
-    static boolean fail = false;
+    static Sudoku sudoku;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         BorderPane root = new BorderPane();
         TilePane tilePane = new TilePane();
         tilePane.setPrefColumns(9);
         tilePane.setPrefRows(9);
+
         HBox hBox = new HBox();
         Button solveButton = new Button();
         solveButton.setText("Solve");
@@ -32,6 +36,7 @@ public class Board extends Application {
 
         textFields = new TextField[9][9];
         intMatrix = new int[9][9];
+        sudoku = new Sudoku(intMatrix);
 
         for (int i = 0; i < intMatrix.length; i++) {
             for (int j = 0; j < intMatrix.length; j++) {
@@ -55,36 +60,20 @@ public class Board extends Application {
 
         }
 
-        ArrayList<Integer> integers = new ArrayList<>();
-        for (int i = 1; i <= 9; i++) {
-            integers.add(i);
-            intMatrix[0][i - 1] = i;
-        }
-        intMatrix[0][3] = 0;
-        intMatrix[0][1] = 0;
-
-        System.out.print("List:   ");
-        integers.forEach(num -> System.out.print(num + " "));
-        System.out.println();
-        System.out.print("Matrix: ");
-        for (int i = 0; i < intMatrix.length; i++) {
-            System.out.print(intMatrix[0][i] + " ");
-        }
-        System.out.println();
-
+        fillFieldsRandomly();
         solveButton.setOnAction(e -> {
-            while (containsZeros(intMatrix[0])) {
-                solveRow(0);
+            for (int i = 0; i < textFields.length; i++) {
+                for (int j = 0; j < textFields.length; j++) {
+                    String text = textFields[i][j].getText();
+                    if (!TextUtils.isEmpty(text)) {
+                        intMatrix[i][j] = Integer.parseInt(text);
+                    }
+                }
             }
-
-            for (int k = 0; k < intMatrix.length; k++) {
-                System.out.print(intMatrix[0][k] + " ");
-            }
-            System.out.println();
-
+            System.out.println("Copied");
+            sudoku.printMatrix();
         });
 
-        System.out.print("New matrix: ");
 
         root.setCenter(tilePane);
         root.setTop(hBox);
@@ -93,32 +82,15 @@ public class Board extends Application {
         primaryStage.show();
     }
 
-    private static void checkRow(ArrayList<Integer> list, int index) {
-        if (!list.isEmpty() && index >= 0) {
-            int current = intMatrix[0][index];
-            if (list.contains(current)) {
-                System.out.println(current);
-//                System.out.println(list.remove(index));
-                list.remove((Object) current);
-                checkRow(list, list.size() - 1);
-            } else {
-                System.out.println("fail");
-            }
-        }
-
-    }
-
-    private static void solveRow(int column) {
-        if (column <= 8) {
-            if (intMatrix[0][column] == 0) {
-                for (int i = 1; i <= 9; i++) {
-                    if (!arrayContains(intMatrix[0], i)) {
-                        intMatrix[0][column] = i;
-                    }
+    private static void fillFieldsRandomly() {
+        for (int i = 0; i < textFields.length; i++) {
+            for (int j = 0; j < textFields.length; j++) {
+                int num = sudoku.getMatrix()[i][j];
+                if (num > 0) {
+                    textFields[i][j].setText(String.valueOf(num));
                 }
-            } else {
-                solveRow(column + 1);
             }
+
         }
     }
 
